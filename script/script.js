5d26810dc9adb6c806fd3e99250d5b8a656ec725
Olwethu Mzileni <s220428336@mandela.ac.za>
2023-08-06 03:08:08 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,35 @@
+const card = document.querySelector(".card");
+const reader = document.querySelector("#reader");
+const btnCopy = document.querySelector(".btn-copy");
+const btnReset = document.querySelector(".btn-reset");
+const resultText = document.querySelector('#result');
+const resultsDisplay = document.querySelector('.text-output');
+const scanner = new Html5QrcodeScanner('reader', {
+    qrbox: {
+        width: 250,
+        height: 250,
+    },
+    fps: 20,
+});
+scanner.render(success, error);
+function success(result){
+    resultText.innerHTML = `<h2>Success!</h2>
+    <textarea>${result}</textarea>`;
+
+    scanner.clear();
+    reader.style.height = "0px"
+    resultsDisplay.style.display = "block";
+}
+
+function error(err){
+    console.error(err)
+}
+
+btnCopy.addEventListener("click", () =>{
+    const decodedText = document.querySelector('#result textarea').textContent;
+    navigator.clipboard.writeText(decodedText); 
+})
+
+btnReset.addEventListener("click", () =>{
+    location.reload();
+})
