4a6d399c8eb32e2e54f5a3142e7613fe5fc4175e
Olwethu Mzileni <s220428336@mandela.ac.za>
2023-08-12 06:18:30 +0200

    Made links active copy btn change added save btn

--- script/script.js
+++ script/script.js
@@ -13,8 +13,20 @@ const scanner = new Html5QrcodeScanner('reader', {
 });
 scanner.render(success, error);
 function success(result){
-    resultText.innerHTML = `<h2>Success!</h2>
-    <textarea>${result}</textarea>`;
+
+    let linkVal = /^https:/;
+    let linkVal2 = /^http:/;
+    let linkVal3 = /^www./;
+
+    if(linkVal.test(result)||linkVal2.test(result)||linkVal3.test(result)){
+        resultText.innerHTML = `<h2>Success!</h2>
+        <div class="textarea"><a href="${result}" style="color:#4717f7;">${result}</a></div>`;
+    }
+    else{
+        resultText.innerHTML = `<h2>Success!</h2>
+        <div class="textarea"><p>${result}</p></div>`;
+    }
+    
 
     scanner.clear();
     reader.style.height = "0px"
@@ -26,8 +38,9 @@ function error(err){
 }
 
 btnCopy.addEventListener("click", () =>{
-    const decodedText = document.querySelector('#result textarea').textContent;
+    const decodedText = document.querySelector('#result .textarea').textContent;
     navigator.clipboard.writeText(decodedText); 
+    btnCopy.innerHTML = '<i class="bi bi-check-all" style="font-size: 32px; color: #130e20;"></i>';
 })
 
 btnReset.addEventListener("click", () =>{
